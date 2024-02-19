dcbe5d7c41ee8efcb514220636b33cfc9eb1ba83
Diego Rodriguez <diego.rodriguez@verygoodsecurity.com>
2024-02-19 00:35:47 -0700

    Allow for EKS support

--- data.tf
+++ data.tf
@@ -94,7 +94,8 @@ data "aws_iam_policy_document" "mwaa" {
     actions = [
       "logs:DescribeLogGroups",
       "cloudwatch:PutMetricData",
-      "s3:GetAccountPublicAccessBlock"
+      "s3:GetAccountPublicAccessBlock",
+      "eks:DescribeCluster
     ]
     resources = [
       "*"
